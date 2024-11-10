445a8e1557989f2f23c871f212b62fdf2ac3a28f
Androidus2 <alexandruturculet@ymail.com>
2024-11-10 20:15:31 +0200

    Initialized the project

---
+++ ASPMicroSocialPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPMicroSocialPlatform.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
